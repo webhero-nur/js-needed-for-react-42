f80939f381b82d0a2307a63416b9cb43a9921bf7
webhero-nur <nurealom522@gmail.com>
2023-01-30 01:33:07 +0600

    JS concepts for react: module summary

---
+++ summary.js
@@ -0,0 +1,44 @@
+/* 
+
+1.1 variable (let, const) three types variable (string, boolean, number)
+1.2 condition (<, >, ===, !==, <=, >=)  &&, ||, if-else, if-else if-else
+1.3 array: declare, length, index, push, pup, indexOf, includes (non existing value in array will give indexOf -1)
+1.4 for loop, while
+1.5 function: declare, return, parameter/argument
+1.6 Object: property, including (array/object)
+
+-------------------------
+
+ES6
+1. template string ${}
+2. spread operator (...)
+2.1 copy an array and then add a new element to an array
+2.2 use filter to remove and element form an array
+3. Arrow function
+3.1 no parameter
+3.2 single parameter
+3.3 multiple parameters ()
+3.4 multi line
+4 destructuring: object destructuring, array destructuring
+5. object declaration shorthand
+6. function parameter default value
+7. Optional chaining ?.
+
+--------------------------
+
+Browser API
+1. localStorage, sessionStorage
+2. location API
+3. History API
+4. fetch
+
+--------------------------
+
+Others
+1. array: map, forEach, filter, find
+2. ternary operator
+3. logical and (&&), logical or (||)
+4. JSON (stringify, parse)
+5. type conversion: (+ --> string to number), ('' --> number to string)
+
+*/
