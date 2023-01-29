7e2a368d13087007a9b150afb9946fa5536f4fba
webhero-nur <nurealom522@gmail.com>
2023-01-30 01:32:06 +0600

    object declaraton shortcut

---
+++ object-shortcut.js
@@ -0,0 +1,23 @@
+const x = 50;
+const y = 100;
+
+const obj = {
+    x: x,
+    y: y
+};
+
+
+const name = 'Ambani';
+const area = 'India';
+
+const nobab = {
+    name: name,
+    area: area
+}
+
+
+
+// shortcut
+const obj1 = { x, y };
+
+const nobab2 = { name, area };
